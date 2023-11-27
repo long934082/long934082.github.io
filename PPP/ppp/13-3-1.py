d6753a1d5aa87d876d505d3ea48828d33b6f4cac
long934082 <145516965+long934082@users.noreply.github.com>
2023-11-27 11:37:43 +0800

    Add files via upload

---
+++ PPP/ppp/13-3-1.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*
+import pandas as pd
+import os
+
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
+print("current dir: " + os.getcwd())
+
+df = pd.read_csv("test.csv")
+print(df)
+df.to_html("ch13-3-1.html")
